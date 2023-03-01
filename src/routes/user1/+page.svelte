27ef904cd6cd39f063e0bb2c0a23ac50d66eb03b
Alexa Steinbrück <5918060+alexabruck@users.noreply.github.com>
2023-03-01 17:24:43 +0100

    Create new routes

---
+++ src/routes/user1/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { confetti } from '@neoconfetti/svelte';
+</script>
+
+<div
+	style="position: absolute; left: 50%; top: 30%"
+	use:confetti={{
+		particleCount: undefined,
+		force: 0.7,
+		stageWidth: window.innerWidth,
+		stageHeight: window.innerHeight,
+		colors: ['#ff3e00', '#40b3ff', '#676778']
+	}}
+/>
