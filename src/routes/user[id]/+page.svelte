2737f2b58d2d0496691d5726d785cd2526db9e03
Alexa Steinbrück <5918060+alexabruck@users.noreply.github.com>
2023-03-04 18:56:50 +0100

    Disentangle states of shared routes

--- src/routes/user[id]/+page.svelte
+++ src/routes/user[id]/+page.svelte
@@ -1,35 +1,8 @@
 <script>
-	import { io } from 'socket.io-client';
-	import Confetti from '../Confetti.svelte';
 	import { page } from '$app/stores';
-
-	let prompt = '';
-	let imageUrl = '';
-	export let celebrate = false;
-	const socket = io();
-	socket.on('winner', (winnerId) => {
-		if (String(winnerId) === $page.params.id) {
-			celebrate = true;
-			setTimeout(() => {
-				celebrate = false;
-			}, 4000);
-		}
-	});
-
-	async function submit() {
-		const response = await fetch('/api/txt2img', {
-			method: 'POST',
-			body: JSON.stringify({ prompt }),
-			headers: { 'content-type': 'application/json' }
-		});
-		const jsonData = await response.json();
-		imageUrl = jsonData.data[0].url;
-	}
+	import ContestantView from './ContestantView.svelte';
 </script>
 
-{#if celebrate}
-	<Confetti />
-{/if}
-<input bind:value={prompt} />
-<img src={imageUrl} />
-<button on:click={submit} type="submit">Submit</button>
+{#key $page.url.pathname}
+	<ContestantView />
+{/key}
---
+++ src/routes/user[id]/ContestantView.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+	import { io } from 'socket.io-client';
+	import Confetti from '../Confetti.svelte';
+	import { page } from '$app/stores';
+
+	const dispatch = createEventDispatcher();
+	let prompt = '';
+	let imageUrl = '';
+
+	export let celebrate = false;
+	const socket = io();
+	socket.on('winner', (winnerId) => {
+		if (String(winnerId) === $page.params.id) {
+			celebrate = true;
+			setTimeout(() => {
+				celebrate = false;
+			}, 4000);
+		}
+	});
+
+	async function submit() {
+		const response = await fetch('/api/txt2img', {
+			method: 'POST',
+			body: JSON.stringify({ prompt }),
+			headers: { 'content-type': 'application/json' }
+		});
+		const jsonData = await response.json();
+		imageUrl = jsonData.data[0].url;
+	}
+</script>
+
+{#if celebrate}
+	<Confetti />
+{/if}
+<input bind:value={prompt} />
+<img src={imageUrl} />
+<button on:click={submit} type="submit">Submit</button>
