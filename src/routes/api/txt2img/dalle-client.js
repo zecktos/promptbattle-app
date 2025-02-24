fd0faead0fc8b5e804c94c160de763289bdd97d9
zecktos <56479813+zecktos@users.noreply.github.com>
2025-02-24 18:40:53 +0100

    allow empty prompt and add oopsie img
    show no error with empty prompt and add an image for empty prompt and future error handling

--- src/routes/api/txt2img/dalle-client.js
+++ src/routes/api/txt2img/dalle-client.js
@@ -1,22 +1,29 @@
 import { Configuration, OpenAIApi } from 'openai';
-import { OPENAI_API_KEY, OPENAI_ORG_ID } from '$env/static/private';
+import { OPENAI_API_KEY } from '$env/static/private';
 
 const configuration = new Configuration({
-	organization: OPENAI_ORG_ID,
+	//organization: OPENAI_ORG_ID,
 	apiKey: OPENAI_API_KEY
 });
 const openai = new OpenAIApi(configuration);
 
 export async function createImage(prompt) {
-	console.log('****** Prompt******: ', prompt);
-	if (!OPENAI_API_KEY) throw Error('OPENAI_API_KEY missing!');
-	if (!prompt) throw Error('Prompt is missing'); //Fix how to do error-stuff in Sveltekit?
-	console.log('...Calling Dalle API...');
-	const response = await openai.createImage({
-		prompt: prompt,
-		n: 2,
-		size: '1024x1024'
-	});
-	const imageUrlPublic = response.data.data[0].url;
-	return { url: imageUrlPublic };
+	if (prompt) {
+		console.log('****** Prompt******: ', prompt);
+		if (!OPENAI_API_KEY) throw Error('OPENAI_API_KEY missing!');
+		//if (!prompt) throw Error('Prompt is missing'); //Fix how to do error-stuff in Sveltekit?
+		console.log('...Calling Dalle API...');
+		const response = await openai.createImage({
+			//model: "dall-e-3",
+			prompt: prompt,
+			n: 1,
+			size: '1024x1024'
+		});
+		const imageUrlPublic = response.data.data[0].url
+		return { url: imageUrlPublic };
+	}
+	else{
+		console.log("######## empty Promtp return no img #########");
+		return { url: "/oopsie.png"};
+	}
 }
