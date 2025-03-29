5895b233505cf66843b3edb4b3bc58f85905d085
zecktos <56479813+zecktos@users.noreply.github.com>
2025-03-29 20:22:02 +0100

    update openai npm package + implement image selection for dalle-backend

--- src/routes/api/txt2img/dalle-client.js
+++ src/routes/api/txt2img/dalle-client.js
@@ -1,11 +1,9 @@
-import { Configuration, OpenAIApi } from 'openai';
+//import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from "openai";
 import { OPENAI_API_KEY } from '$env/static/private';
 
-const configuration = new Configuration({
-	//organization: OPENAI_ORG_ID,
-	apiKey: OPENAI_API_KEY
-});
-const openai = new OpenAIApi(configuration);
+
+const openai = new OpenAI({apiKey: OPENAI_API_KEY});
 
 export async function createImage(prompt) {
 	if (prompt) {
@@ -13,17 +11,21 @@ export async function createImage(prompt) {
 		if (!OPENAI_API_KEY) throw Error('OPENAI_API_KEY missing!');
 		//if (!prompt) throw Error('Prompt is missing'); //Fix how to do error-stuff in Sveltekit?
 		console.log('...Calling Dalle API...');
-		const response = await openai.createImage({
-			//model: "dall-e-3",
+		const response = await openai.images.generate({
+			model: "dall-e-2",
 			prompt: prompt,
-			n: 1,
+			n: 2,
 			size: '1024x1024'
 		});
-		const imageUrlPublic = response.data.data[0].url
-		return { url: imageUrlPublic };
+		//const imageUrlPublic = response.data.data[0].url
+		let finalImages = [];
+		for(let i = 0; i < response.data.length; i++){
+			finalImages.push(response.data[i].url);
+		}
+		return { images: finalImages};
 	}
 	else{
-		console.log("######## empty Promtp return no img #########");
-		return { url: "/oopsie.png"};
+		console.log("######## empty Promtp return no img #########")
+		return { images: ["/oopsie.png"]};
 	}
 }
