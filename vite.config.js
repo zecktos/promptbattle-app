bfcbb45cff1c9b3d0023355f1e49f6b7ab766b8c
zecktos <56479813+zecktos@users.noreply.github.com>
2026-06-11 20:01:20 +0000

    missing changes from prev commit

--- vite.config.js
+++ vite.config.js
@@ -5,38 +5,38 @@ import { Server } from 'socket.io';
 process.env.BROWSER = 'firefox';
 
 const webSocketServerPlugin = {
-	name: 'sveltekit-socket-io',
-	configureServer(server) {
-		const io = new Server(server.httpServer);
-		console.log('SocketIO injected');
+  name: 'sveltekit-socket-io',
+  configureServer(server) {
+    const io = new Server(server.httpServer);
+    console.log('SocketIO injected');
 
-		// server-side
-		io.on('connection', (socket) => {
-			socket.on('promptChange', ({ userId, prompt }) => {
-				io.emit('promptChange', { userId: userId, prompt: prompt });
-			});
-			socket.on('imagesReady', ({ userId, images }) => {
-				io.emit('imagesReady', { userId: userId, images: images });
-			});
-			socket.on('imgSelected', ({ userId, imageUrl }) => {
-				io.emit('imgSelected', { userId: userId, imageUrl: imageUrl });
-			});
-			socket.on('celebrate', ({ userId }) => {
-				io.emit('celebrate', userId);
-			});
-			socket.on('generate', ({ userId }) => {
-				io.emit('generate', userId);
-			});
-			socket.on('reset', ({ userId }) => {
-				io.emit('reset', userId);
-			});
-		});
-	}
+    // server-side
+    io.on('connection', (socket) => {
+      socket.on('promptChange', ({ userId, prompt }) => {
+        io.emit('promptChange', { userId: userId, prompt: prompt });
+      });
+      socket.on('imagesReady', ({ userId, images }) => {
+        io.emit('imagesReady', { userId: userId, images: images });
+      });
+      socket.on('imgSelected', ({ userId, imageUrl }) => {
+        io.emit('imgSelected', { userId: userId, imageUrl: imageUrl });
+      });
+      socket.on('celebrate', ({ userId }) => {
+        io.emit('celebrate', userId);
+      });
+      socket.on('generate', ({ userId, engine }) => {
+        io.emit('generate', { userId: userId, engine: engine });
+      });
+      socket.on('reset', ({ userId }) => {
+        io.emit('reset', userId);
+      });
+    });
+  }
 };
 
 export default defineConfig({
-	plugins: [sveltekit(), webSocketServerPlugin],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  plugins: [sveltekit(), webSocketServerPlugin],
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  }
 });
